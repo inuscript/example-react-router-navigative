9871c2a9925fae5cdb0c44a9212eecf47b018de8
inuscript <inuscript@gmail.com>
2016-05-18 20:50:32 +0900

    magic

--- index.js
+++ index.js
@@ -1,24 +1,40 @@
 import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 import ready from 'doc-ready'
+import { Router, Route, browserHistory, IndexRoute } from 'react-router'
+
 const start = () => {
   return (<div>
       <input />
     </div>
   )
 }
-
+const Home = () => {
+  return <div>Home </div>
+}
 class App extends Component{
   constructor(){
     super()
     this.state = {}
   }
   render(){
-    return <div>hello</div>
+    return <div>
+      <div>hello</div>
+      {this.props.children}
+    </div>
   }
 }
-ready( () => {
+
+const Root = () => (
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={Home} />
+    </Route>
+  </Router>
+)
+
+ ready( () => {
   let container = document.querySelector('#cnt')
   console.log(container)
-  ReactDOM.render(<App />, container)
+  ReactDOM.render(<Root />, container)
 })
