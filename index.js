7ebc64b46bc9e7a3621d279fcabf2941ba2e7629
inuscript <inuscript@gmail.com>
2016-05-18 09:28:46 +0900

    init

---
+++ index.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+const start = () => {
+  return (<div>
+      <input />
+    </div>
+  )
+}
+
+class App extends Component{
+  constructor(){
+    this.super()
+    this.state = {}
+  }
+  render(){
+    
+  }
+}
+
+ReactDOM.render('.cnt', <App />)
